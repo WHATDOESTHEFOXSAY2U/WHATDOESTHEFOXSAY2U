c65ed48c2580c31747d9b8990a156fef900d5a32
Parth_dave <daveparthn17@gmail.com>
2024-04-07 21:59:41 -0400

    Update fetch_youtube_stats.py

--- fetch_youtube_stats.py
+++ fetch_youtube_stats.py
@@ -19,15 +19,20 @@ def update_readme(stats):
     with open('README.md', 'r') as file:
         readme_contents = file.read()
 
-    # Construct the new URLs for badges
-    subs_badge_url = f"https://img.shields.io/badge/dynamic/json?color=blue&label=Subscribers&query=subscriberCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fsubscribers%3FchannelID%3D{CHANNEL_ID}"
-    views_badge_url = f"https://img.shields.io/badge/dynamic/json?color=green&label=Views&query=viewCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fviews%3FchannelID%3D{CHANNEL_ID}"
-    videos_badge_url = f"https://img.shields.io/badge/dynamic/json?color=red&label=Videos&query=videoCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fvideos%3FchannelID%3D{CHANNEL_ID}"
+    # Update URLs directly in README.md content
+    new_subscribers_badge = f"https://img.shields.io/badge/dynamic/json?color=blue&label=Subscribers&query=$.subscriberCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fsubscribers%3FchannelID%3D{CHANNEL_ID}&style=for-the-badge&logo=YouTube"
+    new_views_badge = f"https://img.shields.io/badge/dynamic/json?color=important&label=Views&query=$.viewCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fviews%3FchannelID%3D{CHANNEL_ID}&style=for-the-badge&logo=YouTube"
+    new_videos_badge = f"https://img.shields.io/badge/dynamic/json?color=red&label=Videos&query=$.videoCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fvideos%3FchannelID%3D{CHANNEL_ID}&style=for-the-badge&logo=YouTube"
 
-    # Replace placeholders in the README contents
-    readme_contents = readme_contents.replace('placeholder_url_subscribers', subs_badge_url)
-    readme_contents = readme_contents.replace('placeholder_url_views', views_badge_url)
-    readme_contents = readme_contents.replace('placeholder_url_videos', videos_badge_url)
+    # Pattern to find within the README to replace with new URLs
+    subscriber_pattern = "![Subscribers](https://img.shields.io/badge/dynamic/json?color=blue&label=Subscribers&query=$.subscriberCount&url="
+    view_pattern = "![Views](https://img.shields.io/badge/dynamic/json?color=important&label=Views&query=$.viewCount&url="
+    videos_pattern = "![Videos](https://img.shields.io/badge/dynamic/json?color=red&label=Videos&query=$.videoCount&url="
+
+    # Replacing the old badge URLs with new ones
+    readme_contents = readme_contents.replace(subscriber_pattern, new_subscribers_badge)
+    readme_contents = readme_contents.replace(view_pattern, new_views_badge)
+    readme_contents = readme_contents.replace(videos_pattern, new_videos_badge)
 
     with open('README.md', 'w') as file:
         file.write(readme_contents)
