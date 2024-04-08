cfdc1d326a8b2f95b3e6668c19a5f231e4fe62a3
Parth_dave <daveparthn17@gmail.com>
2024-04-07 22:07:25 -0400

    Update fetch_youtube_stats.py

--- fetch_youtube_stats.py
+++ fetch_youtube_stats.py
@@ -4,6 +4,7 @@ import requests
 
 API_KEY = os.getenv('YOUTUBE_API_KEY')
 CHANNEL_ID = 'UCjf4X2qXcTHa7KoN3m7VKCg'
+print(API_KEY, CHANNEL_ID)
 API_URL = f"https://www.googleapis.com/youtube/v3/channels?part=statistics&id={CHANNEL_ID}&key={API_KEY}"
 
 def fetch_stats():
