3652cf1219a79eaed45187e312ed974b1e3adb9b
Parth_dave <daveparthn17@gmail.com>
2024-04-07 22:34:51 -0400

    Update fetch_youtube_stats.py

--- fetch_youtube_stats.py
+++ fetch_youtube_stats.py
@@ -20,19 +20,13 @@ def update_readme(stats):
     with open('README.md', 'r') as file:
         readme_contents = file.read()
 
-    # Define new badge URLs with Markdown image syntax
-    new_subscribers_badge = "![Subscribers](https://img.shields.io/badge/dynamic/json?color=blue&label=Subscribers&query=$.subscriberCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fsubscribers%3FchannelID%3D" + CHANNEL_ID + "&style=for-the-badge&logo=YouTube)"
-    new_views_badge = "![Views](https://img.shields.io/badge/dynamic/json?color=important&label=Views&query=$.viewCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fviews%3FchannelID%3D" + CHANNEL_ID + "&style=for-the-badge&logo=YouTube)"
-    new_videos_badge = "![Videos](https://img.shields.io/badge/dynamic/json?color=red&label=Videos&query=$.videoCount&url=https%3A%2F%2Fyoutube-channel-stats.vercel.app%2Fapi%2Fvideos%3FchannelID%3D" + CHANNEL_ID + "&style=for-the-badge&logo=YouTube)"
-
-    # Direct string replacement based on the placeholders or existing URLs
-    # Ensure these placeholders or parts of the existing URLs are unique and identifiable
-    readme_contents = readme_contents.replace("EXISTING_SUBSCRIBERS_BADGE_URL", new_subscribers_badge)
-    readme_contents = readme_contents.replace("EXISTING_VIEWS_BADGE_URL", new_views_badge)
-    readme_contents = readme_contents.replace("EXISTING_VIDEOS_BADGE_URL", new_videos_badge)
+    # Replace 'Number' with actual stats in the badge URLs
+    new_contents = readme_contents.replace("Subscribers-Number", f"Subscribers-{stats['subscriberCount']}")
+    new_contents = new_contents.replace("Views-Number", f"Views-{stats['viewCount']}")
+    new_contents = new_contents.replace("Videos-Number", f"Videos-{stats['videoCount']}")
 
     with open('README.md', 'w') as file:
-        file.write(readme_contents)
+        file.write(new_contents)
 
 
 if __name__ == "__main__":
